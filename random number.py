8d5f1056eb3e3bb31cee941696ecf0e3d8b5c83c
Ujjawal593 <113676525+Ujjawal593@users.noreply.github.com>
2024-06-27 18:40:33 +0530

    Delete random number.py

--- random number.py
+++
@@ -1,9 +0,0 @@
-import random
-a = random.randint(1,100)
-b = int(input("please enter your number "))
-if b<a :
-        print("your number is less than",a)
-elif a<b :
-        print("your number is greater than ",a)
-else :
-        print ("your number is equal to ",a)
